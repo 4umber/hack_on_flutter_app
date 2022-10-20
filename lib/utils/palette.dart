393a2783803e4f2689426635e43253c14f49c516
Ci Asto <some@mail.com>
2022-10-20 13:19:26 +0300

    [Utils] Add Palette
    Signed-off-by: Ci Asto <some@mail.com>

---
+++ lib/utils/palette.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static MaterialColor primaryWhite = const MaterialColor(
+    0xffffffff,
+    const <int, Color>{
+      50: Colors.white,
+      100: Colors.white,
+      200: Colors.white,
+      300: Colors.white,
+      400: Colors.white,
+      500: Colors.white,
+      600: Colors.white,
+      700: Colors.white,
+      800: Colors.white,
+      900: Colors.white,
+    },
+  );
+}
